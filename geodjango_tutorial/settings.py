79a4ebd09c428126888149ee3a584668a0964ed3
Karim Bahgat <karim.bahgat.norway@gmail.com>
2016-01-10 11:30:04 +0100

    Update settings.py

--- geodjango_tutorial/settings.py
+++ geodjango_tutorial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'apnnb9)eg^$uiq9yr4jrcsegirt5!ceo$%d87+8x$vt_$c=1xk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = []
 
 
 # Application definition
