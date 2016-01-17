f8b92c8519213a0dc2ccfbf1f7f5414eb3a5ec8b
Karim Bahgat <Karim.bahgat.norway@gmail.com>
2016-01-17 05:33:09 +0100

    Site visuals tweaking

--- pshapes_site/urls.py
+++ pshapes_site/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+admin.site.site_header = "Pshapes User Site"
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url('^$', "provshapes.views.mapview")
