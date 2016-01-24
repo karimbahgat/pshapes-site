e584d49fe8a7ae0e3bf44cd18c1e91aadb541f4a
Karim Bahgat <Karim.bahgat.norway@gmail.com>
2016-01-24 16:44:51 +0100

    dynamic wms input now works

--- provchanges/views.py
+++ provchanges/views.py
@@ -184,12 +184,78 @@ class ToChangeForm(forms.ModelForm):
 
 from django.contrib.gis.forms.widgets import OpenLayersWidget
 
+##class GeoChangeForm(forms.ModelForm):
+##
+##    class Meta:
+##        model = ProvChange
+##        fields = ["transfer_source","transfer_geom"]
+##        widgets = {'transfer_geom': OpenLayersWidget()}
+
 class GeoChangeForm(forms.ModelForm):
 
     class Meta:
         model = ProvChange
         fields = ["transfer_source","transfer_geom"]
-        widgets = {'transfer_geom': OpenLayersWidget()}
+        
+    def __init__(self, *args, **kwargs):
+        super(GeoChangeForm, self).__init__(*args, **kwargs)
+
+        # autozoom map to country depending on country
+##        self.fields['country'].widget.attrs.update({
+##            'onchange': "".join(["var cntr = document.getElementById('id_country').value;",
+##                                 #"alert(cntr);",
+##                                 "var bbox = [0,0,180,90];", #%s[cntr];" % dict([(c.iso3,getbox(c)) for c in pc.all_countries() if getbox(c)]),
+##                                 #"alert(bbox);",
+##                                 "geodjango_transfer_geom.map.zoomToExtent(bbox);",
+##                                ])
+##            })
+
+        # TODO: Also alter required status dynamically
+
+##        # hide map widgets on startup
+##        self.fields['sourceurl'].widget.attrs.update({"style":"display:none"})
+##        self.fields['changepart'].widget.attrs.update({"style":"display:none"}) # grabbing wrong widget so not yet working
+##
+##        # show/hide map widget depending on changetype
+##        self.fields['changetype'].widget.attrs.update({
+##            'onchange': "".join(["var changetype = document.getElementById('id_changetype').value;",
+##                                "if (changetype == 'PartTransfer') ",
+##                                "{",
+##                                "document.getElementById('id_changepart_admin_map').style.display = 'block';",
+##                                "document.getElementById('id_sourceurl').style.display = 'block';",
+##                                "} ",
+##                                "else {",
+##                                "document.getElementById('id_changepart_admin_map').style.display = 'none';",
+##                                "document.getElementById('id_sourceurl').style.display = 'none';",
+##                                "};",
+##                                ])
+##            })
+
+        # make wms auto add/update on sourceurl input
+        self.fields['transfer_source'].widget.attrs.update({
+            'oninput': "".join(["var wmsurl = document.getElementById('id_transfer_source').value;",
+                                "var layerlist = geodjango_transfer_geom.map.getLayersByName('Custom WMS');",
+                                "if (layerlist.length >= 1) ",
+                                "{",
+                                "layerlist[0].url = wmsurl;",
+                                "} ",
+                                "else {",
+                                """customwms = new OpenLayers.Layer.WMS("Custom WMS", wmsurl, {layers: 'basic'} );""",
+                                """customwms.isBaseLayer = false;""",
+                                """geodjango_transfer_geom.map.addLayer(customwms);""",
+                                "};",
+                                ])
+
+            # http://mapwarper.net/maps/wms/11512?request=GetMap&version=1.1.1&format=image/png
+            #'onclick': """geodjango_changepart.map.layers.sourceurl = new OpenLayers.Layer.WMS("Custom WMS","http://mapwarper.net/maps/wms/11512?request=GetMap&version=1.1.1&format=image/png", {layers: 'basic'} ); geodjango_changepart.map.addLayer(geodjango_changepart.map.layers.sourceurl);"""
+            #'onclick': """window.open ("http://www.javascript-coder.com","mywindow","menubar=1,resizable=1,width=350,height=250");"""
+            #'onclick': """alert(geodjango_changepart.map)"""
+            #'onclick': """alert(Object.getOwnPropertyNames(geodjango_changepart.map))"""
+            
+        })
+
+
+
 
 
 #################################################
